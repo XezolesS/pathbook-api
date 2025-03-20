733fdd0004fbcf44eecb21b2fd2fa2f6fc3cd712
XezolesS <tndid7876@gmail.com>
2025-03-20 20:00:02 +0900

    이니셜 커밋.

---
+++ src/main/java/com/pathbook/pathbook_api/PathbookApiApplication.java
@@ -0,0 +1,13 @@
+package com.pathbook.pathbook_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathbookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathbookApiApplication.class, args);
+	}
+
+}
