59ec016933d2beb33aec00f5ff16cefe21dc97e6
XezolesS <tndid7876@gmail.com>
2025-03-28 23:10:29 +0900

    user 스키마 수정.

--- db/init_table_user.sql
+++ db/init_table_user.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `user` (
     `id` VARCHAR(32) NOT NULL,
     `username` VARCHAR(32) NOT NULL,
-    `email` VARCHAR(320) NOT NULL,
+    `email` VARCHAR(320) NOT NULL UNIQUE,
     `password` VARCHAR(64) NOT NULL,
-    `verified` BOOLEAN NOT NULL,
+    `verified` BOOLEAN NOT NULL DEFAULT 0,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
