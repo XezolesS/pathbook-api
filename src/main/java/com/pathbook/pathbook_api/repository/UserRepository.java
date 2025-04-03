22c9031157da85b6cd5a2c90aee46091daae3222
choijuh <506504@naver.com>
2025-04-03 17:29:01 +0900

    회원가입, 이메일 인증

--- src/main/java/com/pathbook/pathbook_api/repository/UserRepository.java
+++ src/main/java/com/pathbook/pathbook_api/repository/UserRepository.java
@@ -19,6 +19,4 @@ public interface UserRepository extends CrudRepository<User, String> {
     // TODO: DB 커넥션 체크용, 프로덕션에서는 지워야 함.
     @Query(value = "SELECT 1 FROM user LIMIT 1", nativeQuery = true)
     int testTableAccess();
-
-    String verificationToken(String verificationToken);
 }
