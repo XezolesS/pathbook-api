1e64a557549c9ff480d350f362c0df43abdb38f4
choijuh <506504@naver.com>
2025-04-03 18:15:34 +0900

    회원가입, 이메일 인증

--- src/main/java/com/pathbook/pathbook_api/controller/AuthController.java
+++ src/main/java/com/pathbook/pathbook_api/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.pathbook.pathbook_api.controller;
 
-import com.pathbook.pathbook_api.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
