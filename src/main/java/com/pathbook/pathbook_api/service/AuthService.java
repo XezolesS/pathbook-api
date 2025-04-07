b245ea2cb24a39944157cb23414a4452053233f6
XezolesS <tndid7876@gmail.com>
2025-04-07 20:28:08 +0900

    불필요한 메서드 삭제.

--- src/main/java/com/pathbook/pathbook_api/service/AuthService.java
+++ src/main/java/com/pathbook/pathbook_api/service/AuthService.java
@@ -1,25 +1,21 @@
 package com.pathbook.pathbook_api.service;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.util.UUID;
-
-import com.pathbook.pathbook_api.TokenStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pathbook.pathbook_api.TokenStore;
 import com.pathbook.pathbook_api.entity.User;
 import com.pathbook.pathbook_api.repository.UserRepository;
-
-import jakarta.servlet.http.HttpSession;
 
 @Service
 public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private EmailService emailService;
+
     @Autowired
     private TokenStore tokenStore;
 
@@ -30,16 +26,6 @@ public class AuthService {
         } catch (Exception ex) {
             return -1;
         }
-    }
-
-    public boolean login(String email, String password, HttpSession session) {
-        User user = userRepository.findByEmail(email);
-        if (user != null && user.getPassword().equals(hashPassword(password))) {
-            session.setAttribute("userId", user.getId());
-            return true;
-        }
-
-        return false;
     }
 
     public String register(String id, String username, String email, String password) {
@@ -77,26 +63,6 @@ public class AuthService {
         }
 
         return false;
-    }
-
-    private String hashPassword(String password) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-            StringBuilder hexString = new StringBuilder();
-
-            for (int i = 0; i < hash.length; i++) {
-                final String hex = Integer.toHexString(0xff & hash[i]);
-
-                if (hex.length() == 1)
-                    hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
     }
 
 }
