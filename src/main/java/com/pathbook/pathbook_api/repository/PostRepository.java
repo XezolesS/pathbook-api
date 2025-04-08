febeb7ed54c102a09b52bdaa10987e1e72f5dc44
Joe K. <tndid7876@gmail.com>
2025-04-08 23:22:50 +0900

    글 작성 프로토타입 (#6)
    * 포스트 작성/조회 프로토타입.
    
    * 포스트 삭제 프로토타입.

---
+++ src/main/java/com/pathbook/pathbook_api/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.pathbook.pathbook_api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pathbook.pathbook_api.entity.Post;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+    
+    Post findById(long id);
+
+}
