bbcf44dc80b2d27089c3e0f027e17db9a308a05e
XezolesS <tndid7876@gmail.com>
2025-04-14 11:23:09 +0900

    와일드카드 임포트 삭제.

--- src/main/java/com/pathbook/pathbook_api/controller/BookmarkController.java
+++ src/main/java/com/pathbook/pathbook_api/controller/BookmarkController.java
@@ -1,18 +1,24 @@
 package com.pathbook.pathbook_api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.pathbook.pathbook_api.entity.Bookmark;
 import com.pathbook.pathbook_api.entity.Post;
 import com.pathbook.pathbook_api.entity.User;
-import com.pathbook.pathbook_api.repository.UserRepository;
 import com.pathbook.pathbook_api.repository.PostRepository;
+import com.pathbook.pathbook_api.repository.UserRepository;
 import com.pathbook.pathbook_api.service.BookmarkService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/bookmark")
--- src/main/java/com/pathbook/pathbook_api/controller/CommentController.java
+++ src/main/java/com/pathbook/pathbook_api/controller/CommentController.java
@@ -1,14 +1,20 @@
 package com.pathbook.pathbook_api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.pathbook.pathbook_api.dto.CommentRequest;
 import com.pathbook.pathbook_api.entity.Comment;
 import com.pathbook.pathbook_api.service.CommentService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/comment")
