e41daa13d8e75c06fc16f1fe8a7b092abb5957e9
XezolesS <tndid7876@gmail.com>
2025-04-14 11:42:17 +0900

    불필요한 코드 삭제.

--- src/main/java/com/pathbook/pathbook_api/repository/PostRepository.java
+++ src/main/java/com/pathbook/pathbook_api/repository/PostRepository.java
@@ -1,15 +1,9 @@
 package com.pathbook.pathbook_api.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pathbook.pathbook_api.entity.Post;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
-
-    Optional<Post> findById(Long id);
-
-}
+public interface PostRepository extends CrudRepository<Post, Long> {}
