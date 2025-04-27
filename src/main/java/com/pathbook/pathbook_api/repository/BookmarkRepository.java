2f6d7d26b7d8f9e8fedb7b819ea39839b6ef6e43
XezolesS <tndid7876@gmail.com>
2025-04-28 03:26:44 +0900

    리팩토링

--- src/main/java/com/pathbook/pathbook_api/repository/BookmarkRepository.java
+++ src/main/java/com/pathbook/pathbook_api/repository/BookmarkRepository.java
@@ -8,8 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pathbook.pathbook_api.entity.Bookmark;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
     boolean existsByUserIdAndPostId(String userId, Long postId);
+
     Optional<Bookmark> findByUserIdAndPostId(String userId, Long postId);
+
     List<Bookmark> findAllByUserId(String userId);
+
     void deleteByUserIdAndPostId(String userId, Long postId);
+
 }
