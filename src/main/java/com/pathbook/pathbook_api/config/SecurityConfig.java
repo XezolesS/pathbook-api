9e0310f8a7e515b8a0bf0a9b7cb0e5e33a6c3948
XezolesS <tndid7876@gmail.com>
2025-05-19 19:24:06 +0900

    프록시 엔드포인트 추가.

--- src/main/java/com/pathbook/pathbook_api/config/SecurityConfig.java
+++ src/main/java/com/pathbook/pathbook_api/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig {
             "/post/{id}",
 
             "/comment/list/{postId}",
+
+            "/proxy/**"
     };
 
     @Bean
@@ -90,7 +92,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         // Make the below setting as * to allow connection from any host
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of(
+                "http://localhost:5173"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(List.of("*"));
---
+++ src/main/java/com/pathbook/pathbook_api/controller/ProxyController.java
@@ -0,0 +1,33 @@
+package com.pathbook.pathbook_api.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/proxy")
+public class ProxyController {
+
+    @GetMapping("/image")
+    public ResponseEntity<?> getImage(@RequestParam String url) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<byte[]> response = restTemplate.getForEntity(url, byte[].class);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_PNG);
+            // headers.setAccessControlAllowOrigin("*");
+
+            return new ResponseEntity<>(response.getBody(), headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+}
