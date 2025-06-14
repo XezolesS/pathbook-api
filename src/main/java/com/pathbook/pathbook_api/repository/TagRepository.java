8367d6b4728f3f9e798fb5f37b6081147848583d
XezolesS <tndid7876@gmail.com>
2025-06-15 01:33:29 +0900

    Repository 추가
    기능 미구현

---
+++ src/main/java/com/pathbook/pathbook_api/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.pathbook.pathbook_api.repository;
+
+import com.pathbook.pathbook_api.entity.Tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {}
