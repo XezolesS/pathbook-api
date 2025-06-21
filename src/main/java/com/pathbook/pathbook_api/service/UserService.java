c02c9ff7b93af092d30dfc512a4db53249672d27
XezolesS <tndid7876@gmail.com>
2025-06-21 18:13:14 +0900

    사용자 정보 반환 추가
    /user/{userId} [GET]

--- src/main/java/com/pathbook/pathbook_api/service/UserService.java
+++ src/main/java/com/pathbook/pathbook_api/service/UserService.java
@@ -1,82 +1,29 @@
-// package com.pathbook.pathbook_api.service;
+package com.pathbook.pathbook_api.service;
 
-// import com.pathbook.pathbook_api.dto.ImageDto;
-// import com.pathbook.pathbook_api.dto.response.UserResponse;
-// import com.pathbook.pathbook_api.entity.User;
-// import com.pathbook.pathbook_api.exception.UserNotFoundException;
-// import com.pathbook.pathbook_api.repository.UserRepository;
+import com.pathbook.pathbook_api.dto.response.UserResponse;
+import com.pathbook.pathbook_api.entity.User;
+import com.pathbook.pathbook_api.exception.UserNotFoundException;
+import com.pathbook.pathbook_api.repository.UserRepository;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class UserService {
-//     @Autowired private UserRepository userRepository;
+@Service
+public class UserService {
+    @Autowired private UserRepository userRepository;
 
-//     public UserResponse getUser(String userId) {
-//         User user =
-//                 userRepository
-//                         .findById(userId)
-//                         .orElseThrow(
-//                                 () ->
-//                                         new UserNotFoundException(
-//                                                 "Cannot find user with id: " + userId));
+    /**
+     * 사용자 정보를 반환합니다.
+     *
+     * @param userId
+     * @return
+     */
+    public UserResponse getUser(String userId) {
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> UserNotFoundException.withUserId(userId));
 
-//         return new UserResponse(
-//                 user.getId(), user.getUsername(), user.getEmail(), user.isVerified());
-//     }
-
-//     public ImageDto getUserIcon(String userId) {
-//         User user =
-//                 userRepository
-//                         .findById(userId)
-//                         .orElseThrow(
-//                                 () ->
-//                                         new UserNotFoundException(
-//                                                 "Cannot find user with id: " + userId));
-
-//         return new ImageDto(user.getIcon(), user.getIconContentType());
-//     }
-
-//     public ImageDto getUserBanner(String userId) {
-//         User user =
-//                 userRepository
-//                         .findById(userId)
-//                         .orElseThrow(
-//                                 () ->
-//                                         new UserNotFoundException(
-//                                                 "Cannot find user with id: " + userId));
-
-//         return new ImageDto(user.getBanner(), user.getBannerContentType());
-//     }
-
-//     public void updateUserIcon(String userId, byte[] iconBytes, String iconContentType) {
-//         User user =
-//                 userRepository
-//                         .findById(userId)
-//                         .orElseThrow(
-//                                 () ->
-//                                         new UserNotFoundException(
-//                                                 "Cannot find user with id: " + userId));
-
-//         user.setIcon(iconBytes);
-//         user.setIconContentType(iconContentType);
-
-//         userRepository.save(user);
-//     }
-
-//     public void updateUserBanner(String userId, byte[] bannerBytes, String bannerContentType) {
-//         User user =
-//                 userRepository
-//                         .findById(userId)
-//                         .orElseThrow(
-//                                 () ->
-//                                         new UserNotFoundException(
-//                                                 "Cannot find user with id: " + userId));
-
-//         user.setBanner(bannerBytes);
-//         user.setBannerContentType(bannerContentType);
-
-//         userRepository.save(user);
-//     }
-// }
+        return new UserResponse(user);
+    }
+}
